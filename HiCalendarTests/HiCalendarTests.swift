00ef179f9dfe999f263e731be8289b68a2714ebf
Jerry <iamtotalchan@gmail.com>
2025-08-08 11:51:36 +0800

    Initial Commit

---
+++ HiCalendarTests/HiCalendarTests.swift
@@ -0,0 +1,17 @@
+//
+//  HiCalendarTests.swift
+//  HiCalendarTests
+//
+//  Created by Jerry  on 2025/8/8.
+//
+
+import Testing
+@testable import HiCalendar
+
+struct HiCalendarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
